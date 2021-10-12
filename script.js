e18cba2027223a384c03fe131ff15833fae25768
Arne B <62389012+ArneCode@users.noreply.github.com>
2021-10-12 11:40:22 +0000

    bezier curve

--- script.js
+++ script.js
@@ -13,17 +13,4 @@ function makeEgg(x, y, width, height, centerY) {
   ctx.fillStyle = "red"
   ctx.fill()
 }
-function curve(points) {
-  // move to the first point
-  ctx.moveTo(points[0].x, points[0].y);
-
-
-  for (i = 1; i < points.length - 2; i++) {
-    var xc = (points[i].x + points[i + 1].x) / 2;
-    var yc = (points[i].y + points[i + 1].y) / 2;
-    ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
-  }
-  // curve through the last two points
-  ctx.quadraticCurveTo(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
-}
 makeEgg(100, 100, 55, 100)
