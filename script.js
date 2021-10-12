0a45917c494c6de971a6a155aafa05d90daf1876
Arne B <62389012+ArneCode@users.noreply.github.com>
2021-10-12 13:49:17 +0000

    got egg to work using formula from http://www.mathematische-basteleien.de/eggcurves.htm

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 let canvas = document.getElementById("canvas")
 let ctx = canvas.getContext("2d")
-let width = window.innerWidth / 2
-let height = window.innerHeight / 2
+let width = window.innerWidth
+let height = window.innerHeight
 canvas.width = width
 canvas.height = height
-function makeEgg(x, y, width, height, centerY) {
-  let downOff=20
-  let arr=[
-    {x:x-width,y:y+downOff},
-    {x:x-width/1.5,y:y-height/1.5},
-    {x:x,y:y-height}, 
-    {x:x+width/1.5,y:y-height/1.5},
-    {x:x+width,y:y+downOff},
-    {x:x,y:y+height},
-    {x:x-width,y:y+downOff},
-    {x:x-width/1.5,y:y-height/1.5},
-    {x:x,y:y-height}, 
-
-  ]
+function makeEggPath(xPos,yPos,a,b,d) {
+  let pathL=[]
+  let pathR=[]
+  for(let y=-a;y<=a;y+=1.5){
+    let x=Math.sqrt(((-(b**(2))*(y**(2))+(b**(2))*(a**(2)))/(((a**(2))+2*d*y+(d**(2))))))
+    pathL.push({x:-x+xPos,y:(y*-1)+yPos})
+    pathR.push({x:x+xPos,y:(y*-1)+yPos})
+  }
+  //console.log(pathL)
+  return pathL.concat(pathR.reverse())
+}
+function drawPath(path){
+  console.log(path)
   ctx.beginPath()
-  customCurve(arr)
-  //ctx.closePath()
-  ctx.fillStyle = "red"
+  ctx.moveTo(path[0].x,path[0].y)
+  for(let i in path){
+    ctx.lineTo(path[i].x,path[i].y)
+  }
+  ctx.strokeStyle="black"
+  ctx.stroke()
+  ctx.beginPath()
+  ctx.rect(100,100,3,3)
   ctx.fill()
 }
-function customCurve(points) {
-  // move to the first point
-  ctx.moveTo(points[0].x, points[0].y);
-
-
-  for (i = 1; i < points.length - 2; i++) {
-    var xc = (points[i].x + points[i + 1].x) / 2;
-    var yc = (points[i].y + points[i + 1].y) / 2;
-    ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
-  }
-  // curve through the last two points
-  ctx.quadraticCurveTo(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
-}
-makeEgg(100, 100, 55, 100)
+let b=50+(Math.random()-0.5)*5
+drawPath(makeEggPath(100, 100, 70,b,22))
