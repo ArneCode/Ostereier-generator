76db3ec1a1f781e1531635b7518e536f57e983ad
Arne B <62389012+ArneCode@users.noreply.github.com>
2021-10-13 08:29:46 +0000

    added colors and new split method

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let canvas = document.getElementById("canvas")
 let ctx = canvas.getContext("2d")
-let width = window.innerWidth
-let height = window.innerHeight
+ctx.lineWidth = 0.01
+let width = 800*3
+let height = 800*3
 canvas.width = width
 canvas.height = height
 let methodList = [3, 4]
@@ -16,22 +17,26 @@ function makeEggPath(xPos, yPos, a, b, d) {
   //console.log(pathL)
   return pathL.concat(pathR.reverse())
 }
-function drawPath(path) {
+function drawEgg(path,color) {
   console.log(path)
   ctx.beginPath()
   ctx.moveTo(path[0].x, path[0].y)
   for (let i in path) {
     ctx.lineTo(path[i].x, path[i].y)
   }
+  ctx.lineWidth=3
   ctx.strokeStyle = "black"
   ctx.stroke()
+  ctx.fillStyle=color
+  ctx.fill()
+  ctx.lineWidth=1
 }
-function subDivideEgg(path, methods) {
+function subDivideEgg(path, methods, colors) {
   let length = path.length
   let triangle = [path[0], path[Math.floor(length / 3)], path[Math.floor(length / 3) * 2]]
   let l_half = Math.round(length / 2)
-  let triangle_flipped = [path[0 + l_half], path[Math.floor(length / 3) + Math.round(l_half*1.1111)], path[(Math.floor(length / 3) * 2 + Math.round(l_half*0.9)) % length]]
-  if (true/*Math.random() < 0.5*/) {
+  let triangle_flipped = [path[0 + l_half], path[Math.floor(length / 3) + Math.round(l_half * 1.1111)], path[(Math.floor(length / 3) * 2 + Math.round(l_half * 0.9)) % length]]
+  if (Math.random() < 0.5/**/) {
     //not flipped
     let oTriangles = []
     oTriangles.push([
@@ -49,15 +54,34 @@ function subDivideEgg(path, methods) {
       triangle_flipped[2],
       triangle[0]
     ])
-    drawTriangle(triangle)
-    subDivideTriangle(triangle, methods)
-    for(let t of oTriangles){
-      subDivideTriangle(t,methods)
+    strokeTriangle(triangle)
+    subDivideTriangle(triangle, methods, colors)
+    for (let t of oTriangles) {
+      subDivideTriangle(t, methods, colors)
     }
   } else {
     //flipped
-    drawTriangle(pts)
-    subDivideTriangle(pts, methods)
+    let oTriangles = []
+    oTriangles.push([
+      triangle_flipped[2],
+      triangle[0],
+      triangle_flipped[1]
+    ])
+    oTriangles.push([
+      triangle_flipped[0],
+      triangle[1],
+      triangle_flipped[2]
+    ])
+    oTriangles.push([
+      triangle_flipped[1],
+      triangle[2],
+      triangle_flipped[0]
+    ])
+    strokeTriangle(triangle_flipped)
+    subDivideTriangle(triangle_flipped, methods, colors)
+    for (let t of oTriangles) {
+      subDivideTriangle(t, methods, colors)
+    }
   }
 }
 function ptBetween(a, b) {
@@ -75,7 +99,7 @@ function ptBetween(a, b) {
     return { x: (a.x + b.x) / 2, y: (a.y + b.y) / 2 }
   }
 }
-function subDivideTriangle(pts, methods, step = 0) {
+function subDivideTriangle(pts, methods, colors, step = 0) {
   let method = methods[step]
   let nTriangles = []
   switch (method) {
@@ -124,15 +148,37 @@ function subDivideTriangle(pts, methods, step = 0) {
 
       break;
     }
+    case 2:{
+      nTriangles=splitTriangle2(pts[1],pts[2],pts[0])
+      break;
+    }
   }
-  if (step < methods.length) {
+  if (step+1 < methods.length) {
     for (let triangle of nTriangles) {
-      drawTriangle(triangle)
-      subDivideTriangle(triangle, methods, step + 1)
+      //strokeTriangle(triangle)
+      subDivideTriangle(triangle, methods, colors, step + 1)
+    }
+  } else {
+    for (let i = 0; i < nTriangles.length; i++) {
+      fillTriangle(nTriangles[i], colors[i])
     }
   }
 }
-function drawTriangle(pts) {
+function splitTriangle2(a,b,c){
+  let center=ptBetween(a,b)
+  let t1=[
+    center,
+    a,
+    c
+  ]
+  let t2=[
+    center,
+    b,
+    c
+  ]
+  return [t1,t2]
+}
+function fillTriangle(pts,color){
   ctx.beginPath()
   moveTo(pts[0].x, pts[0].y)
   for (let i = 0; i < pts.length; i++) {
@@ -140,12 +186,24 @@ function drawTriangle(pts) {
     ctx.lineTo(pt.x, pt.y)
   }
   ctx.closePath()
-  console.log(pts)
+  ctx.fillStyle = color
+  ctx.fill()
+}
+function strokeTriangle(pts) {
+  ctx.beginPath()
+  moveTo(pts[0].x, pts[0].y)
+  for (let i = 0; i < pts.length; i++) {
+    let pt = pts[i]
+    ctx.lineTo(pt.x, pt.y)
+  }
+  ctx.closePath()
   ctx.strokeStyle = "black"
   ctx.stroke()
 }
+let scale = 5
 let b = 50 + (Math.random() - 0.5) * 5
-let path = makeEggPath(100, 100, 70, b, 22)
-drawPath(path)
-let methods = [4, 3, 3]
-subDivideEgg(path, methods)
+let path = makeEggPath(b * scale+10, 70 * scale+10, 70 * scale, b * scale, 22 * scale)
+let colors = colourschemes[Math.floor(Math.random() * colourschemes.length)]
+drawEgg(path,colors[Math.floor(Math.random()*3)])
+let methods = [2, 3, 3,4]
+subDivideEgg(path, methods, colors)
